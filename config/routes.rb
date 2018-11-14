8da8776ac974ca2e16f7db161c4ff241670bd0ac
DarkBones <donkerbc@gmail.com>
2018-11-14 20:14:47 +0000

    Create static welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get 'users/show'
   devise_for :users, :controllers => { registrations: 'registrations' }
   resources :users
-  root 'users#index'
+  root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
