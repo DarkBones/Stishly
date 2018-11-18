49b7a9d6fa02cc35b8a9e21c2f0caed0042223a7
DarkBones <donkerbc@gmail.com>
2018-11-18 19:53:00 +0000

    Improve parsing create account requests

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -1,7 +1,7 @@
 class AccountController < ApplicationController
 
   def create
-    reg = "^[a-zA-Z0-9\s]+[0-9]*[\.]*[0-9\.\s]+$"
+    reg = "^[a-zA-Z0-9\s]+[0-9]*[\.,]*[0-9\.\s]+$"
     @name_balance = params[:account][:name_balance].strip
 
     if /#{reg}/.match(@name_balance)
@@ -10,7 +10,7 @@ class AccountController < ApplicationController
 
       @name = @name_balance[0..-2].join(' ')
 
-      @cents_amount > 0 ? @balance = (@name_balance[-1].to_f * @cents_amount).to_i : @balance = @name_balance[-1].to_i
+      @cents_amount > 0 ? @balance = (@name_balance[-1].sub(",", ".").to_f * @cents_amount).to_i : @balance = @name_balance[-1].to_i
 
       Account.create(:user_id => current_user.id, :name => @name, :balance => @balance)
       redirect_to root_path
