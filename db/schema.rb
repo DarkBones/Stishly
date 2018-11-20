a942d7af576b396c8f75bc288d847035eacdfe37
DarkBones <donkerbc@gmail.com>
2018-11-20 20:37:03 +0000

    Add account_id column to transactions table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_18_131732) do
+ActiveRecord::Schema.define(version: 2018_11_20_200800) do
 
   create_table "accounts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "balance"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 2018_11_18_131732) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "account_id"
     t.index ["user_id"], name: "index_transactions_on_user_id"
   end
 
