679960dd91ad763d49d2f542a00bebeab4e5e0d6
DarkBones <donkerbc@gmail.com>
2018-11-21 20:01:26 +0000

    Improve account links

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -1,7 +1,8 @@
 class AccountController < ApplicationController
 
   def show
-    if params[:id] != 'all'
+    @account_id = params[:id]
+    if @account_id != 'all'
       @transactions = Transaction.where("account_id" => params[:id], "user_id" => current_user.id)
     else
       @transactions = Transaction.where("user_id" => current_user.id)
