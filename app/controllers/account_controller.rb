dbd817c26bc73c1e49bfb007b7baa19d21e8a373
DarkBones <donkerbc@gmail.com>
2018-11-23 17:26:19 +0000

    Redirect on account creation

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -11,29 +11,7 @@ class AccountController < ApplicationController
 
   def create
     Account.create_from_string(params[:account][:name_balance].to_s, current_user.id, current_user.country.currency.number_to_basic)
-=begin
-    reg = "^[a-zA-Z0-9\s]+[0-9]*[\.,]*[0-9\.\s]+$"
-    @name_balance = params[:account][:name_balance].strip
-
-    if /#{reg}/.match(@name_balance)
-      @cents_amount = current_user.country.currency.number_to_basic
-      @name_balance = @name_balance.split
-
-      @name = @name_balance[0..-2].join(' ')
-
-      @cents_amount > 0 ? @balance = (@name_balance[-1].sub(",", ".").to_f * @cents_amount).to_i : @balance = @name_balance[-1].to_i
-
-      Account.create(:user_id => current_user.id, :name => @name, :balance => @balance)
-      redirect_to root_path
-    elsif @name_balance.length > 0
-      Account.create(:user_id => current_user.id, :name => @name_balance, :balance => 0)
-      redirect_to root_path
-    else
-      redirect_to root_path, :alert => 'Unexpected input'
-    end
-=end
-
-    #@cents_amount = current_user.country.currency.number_to_basic
+    redirect_back(fallback_location: root_path)
   end
 
   def sort
