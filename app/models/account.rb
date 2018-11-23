e707e850b60ce0fd9509c2d5c83c51652176db3e
DarkBones <donkerbc@gmail.com>
2018-11-23 17:39:33 +0000

    Refactor for readability

--- app/models/account.rb
+++ app/models/account.rb
@@ -15,22 +15,27 @@
 class Account < ApplicationRecord
   belongs_to :user
   has_many :transactions
-  
+
   def self.create_from_string(str, current_user)
     reg = ".+\s+[\.,]*[0-9\.\s]+$"
     @name_balance = str.to_s.strip
     @cents_amount = current_user.country.currency.number_to_basic
+
+    @balance = 0
+    @name = @name_balance
 
     if /#{reg}/.match(@name_balance)
       @name_balance = @name_balance.split
 
       @name = @name_balance[0..-2].join(' ')
 
-      @cents_amount > 0 ? @balance = (@name_balance[-1].sub(",", ".").to_f * @cents_amount).to_i : @balance = @name_balance[-1].to_i
-
-      self.create(:user_id => current_user.id, :name => @name, :balance => @balance)
-    elsif @name_balance.length > 0
-      self.create(:user_id => current_user.id, :name => @name_balance, :balance => 0)
+      if @cents_amount > 0
+        @balance = (@name_balance[-1].sub(",", ".").to_f * @cents_amount).to_i
+      else
+        @balance = @name_balance[-1].to_i
+      end
     end
+
+    self.create(:user_id => current_user.id, :name => @name, :balance => @balance)
   end
 end
