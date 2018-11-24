64e94b8d4aff3780d89738005fc64707c42a1f11
DarkBones <donkerbc@gmail.com>
2018-11-24 21:37:33 +0000

    New styling

--- app/assets/javascripts/accounts.js
+++ app/assets/javascripts/accounts.js
@@ -13,7 +13,7 @@ $(document).on('turbolinks:load', ()=> {
   });
 
   $('.account-button').on('click', (event) => {
-    window.location.href = '/'+event.target.id.replace('_', '/');
+    window.location.href = '/' + event.target.id.replace('_', '/');
   });
 });
 
