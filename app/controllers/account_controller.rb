81f0a514378442a1d3379cbf2a8cadddf751107f
DarkBones <donkerbc@gmail.com>
2018-11-24 13:08:36 +0000

    Change order of transactions

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -2,9 +2,9 @@ class AccountController < ApplicationController
   def show
     @account_id = params[:id]
     if @account_id != 'all'
-      @transactions = Transaction.where("account_id" => params[:id], "user_id" => current_user.id)
+      @transactions = Transaction.where("account_id" => params[:id], "user_id" => current_user.id).order(:created_at).reverse_order()
     else
-      @transactions = Transaction.where("user_id" => current_user.id)
+      @transactions = Transaction.where("user_id" => current_user.id).order(:created_at).reverse_order()
     end
   end
 
--- app/controllers/transaction_controller.rb
+++ app/controllers/transaction_controller.rb
@@ -3,7 +3,12 @@ class TransactionController < ApplicationController
     @user_input = params[:transaction][:user_input].to_s
     @account_id = params[:account_id].to_i
 
-    @result = Transaction.create_from_string(@user_input, current_user, @account_id, -1)
+    # TODO: Create option for direction in the form
+    @direction = -1
 
+    @result = Transaction.create_from_string(@user_input, current_user, @account_id, @direction)
+
+    # TODO: Error handling if @result != 200
+    redirect_back(fallback_location: root_path)
   end
 end
