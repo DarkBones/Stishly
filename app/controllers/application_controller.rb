8400d3acd6d202fa9300c35e1e3dd0d76b294480
DarkBones <donkerbc@gmail.com>
2018-11-26 21:15:11 +0000

    Update user.timezone after login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  private
+
+  def after_sign_in_path_for(resource_or_scope)
+    User.update(current_user.id, :timezone => params[:user][:timezone])
+  end
 end
