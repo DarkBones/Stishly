a8efd4c7d5df733b032c99a8476bc46f4608d6e2
DarkBones <donkerbc@gmail.com>
2018-11-30 19:23:26 +0000

    Refactor transaction creation

---
+++ app/models/transaction/create_from_string.rb
@@ -0,0 +1,53 @@
+class Transaction
+  class CreateFromString
+    def initialize(params, current_user)
+      @transaction_string = params[:transaction_string]
+      @direction = params[:direction]
+      @account_id = params[:account_id]
+      @current_user = current_user
+      @cents_amount = current_user.country.currency.number_to_basic
+    end
+
+    def perform()
+      if @transaction_string.length == 0
+        return 'Please enter a valid transaction'
+      end
+
+      @transaction_details = parse_string()
+
+      return @transaction_details
+    end
+
+    private
+
+    def parse_string()
+      reg = ".+\s+[\.,]*-?[0-9\.]+$"
+
+      name_amount = @transaction_string.strip
+
+      if /#{reg}/.match(name_amount)
+        name_amount = name_amount.split
+
+        transaction_name = name_amount[0..-2].join(' ')
+
+        if @cents_amount > 0
+          amount = (name_amount[-1].sub(",", ".").to_f * @cents_amount).to_i
+        else
+          amount = name_amount[-1].to_f.round.to_i
+        end
+
+        amount *= @direction
+
+        result = { 
+          :user_id => @current_user.id, 
+          :description => transaction_name, 
+          :amount => amount ,
+          :account_id => @account_id,
+          :timezone => @current_user.timezone
+        }
+
+        return result
+      end
+    end
+  end
+end
