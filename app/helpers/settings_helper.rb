a658dc191f237761e8d0e385fa350824e97d0410
DarkBones <donkerbc@gmail.com>
2018-12-04 22:01:44 +0000

    Add currency selector to settings

--- app/helpers/settings_helper.rb
+++ app/helpers/settings_helper.rb
@@ -1,2 +1,9 @@
 module SettingsHelper
+  def all_currencies(hash)
+    array = []
+    hash.each_with_index do |c, i|
+      array.push(["#{c[1][:iso_code]} - #{c[1][:name]}", c[1][:iso_code]])
+    end
+    return array
+  end
 end
