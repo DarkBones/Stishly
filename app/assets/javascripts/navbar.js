9883c38ad06456128b28c9d14a5a25e215dc1d19
DarkBones <donkerbc@gmail.com>
2019-01-11 16:03:40 +0000

    Hide menu after clicking out

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -1,5 +1,10 @@
 $(document).on('turbolinks:load', ()=> {
   $('.navbar__menu-toggle').on('click', (event) => {
-    $('.navbar__menu').toggle();
+    $('.navbar__menu').slideToggle(50);
+    event.stopPropagation();
+  });
+
+  $(document).on('click', (event) => {
+    $('.navbar__menu').slideUp(50);
   });
 });
