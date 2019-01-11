012517ebf89fdef558012ab9fa3d9d3f74e51f84
DarkBones <donkerbc@gmail.com>
2019-01-11 13:31:42 +0000

    Account model tests

--- test/models/account_test.rb
+++ test/models/account_test.rb
@@ -33,6 +33,14 @@ class AccountTest < ActiveSupport::TestCase
     assert_not account.save, format_error("Saved the account without a name")
   end
 
+  test "Save with dots in name" do
+    current_user = users(:bas)
+    account = Account.create_new({:name => 'test.dot.', :currency => 'EUR'}, current_user)
+
+    assert_not account.is_a?(ActiveRecord::Base), format_error("Created account with dots in name")
+    assert account == I18n.t('account.failure.invalid_name_dot'), format_error("Unexpected error", I18n.t('account.failure.invalid_name_dot'), account)
+  end
+
   test "Save account without balance" do
     current_user = users(:bas)
     account = current_user.accounts.build({:name => 'test account'})
@@ -41,53 +49,14 @@ class AccountTest < ActiveSupport::TestCase
     assert account.balance == 0, format_error("Unexpected default account balance", 0, account.balance)
   end
 
-  test "Create from string \"from_string account 9.99\"" do
+  test "Create duplicate account" do
     current_user = users(:bas)
     params = {account_string: 'from_string account 9.99'}
 
-    a1 = Account.create_from_string(params, current_user)
+    a1 = Account.create({:name => 'test duplicate'}, current_user)
+    a2 = Account.create({:name => 'test duplicate'}, current_user)
 
-    assert a1.is_a?(ActiveRecord::Base), format_error("Could not create account from string", result: a1)
-    assert a1.balance == 999, format_error("Unexpected account balance", 999, a1.balance)
-  end
-
-  test "Create twice from string \"from_string account 9.99\"" do
-    current_user = users(:bas)
-    params = {account_string: 'from_string account 9.99'}
-
-    a1 = Account.create_from_string(params, current_user)
-    a2 = Account.create_from_string(params, current_user)
-    
     assert_not a2.is_a?(ActiveRecord::Base), format_error("Created duplicate account name")
-  end
-
-  test "Create from string \"from_string account\"" do
-    current_user = users(:bas)
-    params = {account_string: 'from_string account'}
-
-    a1 = Account.create_from_string(params, current_user)
-
-    assert a1.is_a?(ActiveRecord::Base), format_error("Could not create account from string without specifying the balance", result: a1)
-  end
-
-  test "Convert currency" do
-    # get the user
-    current_user = users(:currency_test)
-
-    # parameters for changing account's currency setting
-    params = {setting_value: {currency: "JPY"}}
-    account = accounts(:currency_1)
-
-    # change account's currency setting
-    a1 = Account.change_setting(account, params, current_user)
-
-    # assert currency has changed to "JPY"
-    currency = Account.get_currency(a1.id, current_user)
-    assert currency == "JPY", format_error("Unexpected account currency after updating setting", "JPY", currency)
-
-    # assert that the transactions that were made today have been converted to the new currency, and transactions that happened earlier were not updated
-    a1.transactions.each do |t|
-      puts t.created_at
-    end
+    assert a2 == I18n.t('account.failure.already_exists'), format_error("Unexpected error", I18n.t('account.failure.already_exists'), a2)
   end
 end
