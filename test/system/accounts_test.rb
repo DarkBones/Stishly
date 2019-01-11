86944f7cdc6addf021394861ae402face48fab6d
DarkBones <donkerbc@gmail.com>
2019-01-11 18:58:52 +0000

    Accounts system tests

--- test/system/accounts_test.rb
+++ test/system/accounts_test.rb
@@ -1,136 +1,87 @@
 require "application_system_test_case"
 
 class AccountsTest < ApplicationSystemTestCase
-  test "create blank account" do
-    """
-    Create a new account with no name
-    Expected result:
-    - See error about invalid account name
-    """
+  test "Account tests" do
 
     login_as_blank
 
+    # Open the new account menu
     page.find("#create-account-button").click
+    # Find and store the submit button
+    submit = page.find(".card-form input[type=submit]")
+    # Check if button is disabled correctly
+    assert submit[:disabled], format_error("Save account button not disabled when name is blank", "disabled = true", "disabled = " + submit[:disabled].to_s)
+    # Fill in an account name
+    fill_in "account[name]", with: "test account one"
+    # Check if the button is enabled correctly
+    assert submit[:disabled] == nil, format_error("Save account button disabled", "disabled = nil", "disabled = " + submit[:disabled].to_s)
+    # Save the account
+    click_on "Save Account"
 
-    click_on "Create"
+    # Open the new account menu
+    page.find("#create-account-button").click
+    # Find and store the submit button
+    submit = page.find(".card-form input[type=submit]")
+    # Fill in the details
+    fill_in "account[name]", with: "test. account. two"
+    fill_in "account[balance]", with: "50sd0df0FD"
+    fill_in "account[description]", with: "Test description"
+    # Save the account
+    click_on "Save Account"
 
-    #take_screenshot
-
-    assert_selector '#flash_alert', text: I18n.t('account.failure.invalid_name')
-  end
-
-  test "create duplicate account" do
-    """
-    Create an account with the same name twice
-    Expected result:
-    - See error about account already existing
-    """
-
-    login_as_blank
-
-    2.times do
-      page.find("#create-account-button").click
-      fill_in "account_account_string", with: "duplicate account 0.00"
-      click_on "Create"
-    end
-
+    # Open the new account menu
+    page.find("#create-account-button").click
+    # Find and store the submit button
+    submit = page.find(".card-form input[type=submit]")
+    # Fill in the same name as the previous account
+    fill_in "account[name]", with: "test. account. two"
+    # Save the account
+    click_on "Save Account"
+    # Check if the duplicate account error shows
     assert_selector '#flash_alert', text: I18n.t('account.failure.already_exists')
 
-    #take_screenshot
-  end
+    # Open the new account menu
+    page.find("#create-account-button").click
+    # Find and store the submit button
+    submit = page.find(".card-form input[type=submit]")
+    # Fill in the details
+    fill_in "account[name]", with: "test account three"
+    fill_in "account[balance]", with: "10000.42"
+    # Save the account
+    click_on "Save Account"
 
-  test "create accounts from string" do
-    """
-    Create new accounts from string
-    Expected result:
-    - See new accounts in left menu
-    """
+    # Open the new account menu
+    page.find("#create-account-button").click
+    # Find and store the submit button
+    submit = page.find(".card-form input[type=submit]")
+    # Fill in the details
+    fill_in "account[name]", with: "test account four"
+    fill_in "account[balance]", with: "50000"
+    select "JPY", from: "account[currency]"
+    # Save the account
+    click_on "Save Account"
 
-    login_as_blank
+    # Open the new account menu
+    page.find("#create-account-button").click
+    # Find and store the submit button
+    submit = page.find(".card-form input[type=submit]")
+    # Fill in the details
+    fill_in "account[name]", with: "test account five"
+    fill_in "account[balance]", with: "0.08"
+    # Save the account
+    click_on "Save Account"
 
-    account_strings = [
-      {
-        string: "Test account",
-        result: "Test account\n€ 0.00"
-      },
-      {
-        string: "Test 10",
-        result: "Test\n€ 10.00"
-      },
-      {
-        string: "Test 1 .10",
-        result: "Test 1\n€ 0.10"
-      },
-      {
-        string: "Test 2 99.66",
-        result: "Test 2\n€ 99.66"
-      },
-      {
-        string: "Test 3 100.06",
-        result: "Test 3\n€ 100.06"
-      },
-      {
-        string: "Test 4 10.5",
-        result: "Test 4\n€ 10.50"
-      },
-      {
-        string: "404",
-        result: "404\n€ 0.00"
-      },
-      {
-        string: "Negative -10.5",
-        result: "Negative\n€ -10.50"
-      },
-      {
-        string: "3.50",
-        result: "3.50\n€ 0.00"
-      },
-      {
-        string: "comma test 3,50",
-        result: "comma test\n€ 3.50"
-      },
-      {
-        string: "many dots test 9.87.65.43.21",
-        result: "many dots test 9.87.65.43.21\n€ 0.00"
-      },
-      {
-        string: "just a bunch of dots ...",
-        result: "just a bunch of dots ...\n€ 0.00"
-      },
-      {
-        string: "99.99",
-        result: "99.99\n€ 0.00"
-      }
-    ]
+    # Check if the new accounts shows up in the left menu
+    #assert_selector '#accounts_list', text: "All\n€ 15,400.50\ntest account one\n€ 0.00\ntest account two\n€ 5,000.00\ntest account three\n€ 10,000.42\ntest account four\n¥ 50,000\ntest account five\n€ 0.08"
 
-    account_strings.each_with_index do |s, i|
-      if i <= 1
-        assert_no_selector "#accounts_list", text: "all"
-      end
+    #page.find(".account-button:nth-child(2)").hover
 
-      page.find("#create-account-button").click
+    #element = page.find(".account-button:nth-child(2) .sort-handle")
+    #target = page.find(".account-button:nth-child(6)")
 
-      fill_in "account_account_string", with: s[:string]
-
-      click_on "Create"
-
-      assert_selector '#accounts_list', text: s[:result]
-    end
-
-    assert_selector '#accounts_list', text: "All\n€ 213.32"
+    #element.drag_to target
 
     #take_screenshot
-  end
-
-  test "Visit account" do
-    user = users(:bas)
-    password = "SomePassword123^!"
-    login_user(user, password)
-
-    account_name = "Current account"
-
-    visit "/account/" + URI.encode(account_name)
-
-    take_screenshot
+    #puts element[:class]
   end
 end
---
+++ test/system/accounts_test_OLD.rb
@@ -0,0 +1,136 @@
+require "application_system_test_case"
+
+class AccountsTest < ApplicationSystemTestCase
+  test "create blank account" do
+    """
+    Create a new account with no name
+    Expected result:
+    - See error about invalid account name
+    """
+
+    login_as_blank
+
+    page.find("#create-account-button").click
+
+    click_on "Create"
+
+    #take_screenshot
+
+    assert_selector '#flash_alert', text: I18n.t('account.failure.invalid_name')
+  end
+
+  test "create duplicate account" do
+    """
+    Create an account with the same name twice
+    Expected result:
+    - See error about account already existing
+    """
+
+    login_as_blank
+
+    2.times do
+      page.find("#create-account-button").click
+      fill_in "account_account_string", with: "duplicate account 0.00"
+      click_on "Create"
+    end
+
+    assert_selector '#flash_alert', text: I18n.t('account.failure.already_exists')
+
+    #take_screenshot
+  end
+
+  test "create accounts from string" do
+    """
+    Create new accounts from string
+    Expected result:
+    - See new accounts in left menu
+    """
+
+    login_as_blank
+
+    account_strings = [
+      {
+        string: "Test account",
+        result: "Test account\n€ 0.00"
+      },
+      {
+        string: "Test 10",
+        result: "Test\n€ 10.00"
+      },
+      {
+        string: "Test 1 .10",
+        result: "Test 1\n€ 0.10"
+      },
+      {
+        string: "Test 2 99.66",
+        result: "Test 2\n€ 99.66"
+      },
+      {
+        string: "Test 3 100.06",
+        result: "Test 3\n€ 100.06"
+      },
+      {
+        string: "Test 4 10.5",
+        result: "Test 4\n€ 10.50"
+      },
+      {
+        string: "404",
+        result: "404\n€ 0.00"
+      },
+      {
+        string: "Negative -10.5",
+        result: "Negative\n€ -10.50"
+      },
+      {
+        string: "3.50",
+        result: "3.50\n€ 0.00"
+      },
+      {
+        string: "comma test 3,50",
+        result: "comma test\n€ 3.50"
+      },
+      {
+        string: "many dots test 9.87.65.43.21",
+        result: "many dots test 9.87.65.43.21\n€ 0.00"
+      },
+      {
+        string: "just a bunch of dots ...",
+        result: "just a bunch of dots ...\n€ 0.00"
+      },
+      {
+        string: "99.99",
+        result: "99.99\n€ 0.00"
+      }
+    ]
+
+    account_strings.each_with_index do |s, i|
+      if i <= 1
+        assert_no_selector "#accounts_list", text: "all"
+      end
+
+      page.find("#create-account-button").click
+
+      fill_in "account_account_string", with: s[:string]
+
+      click_on "Create"
+
+      assert_selector '#accounts_list', text: s[:result]
+    end
+
+    assert_selector '#accounts_list', text: "All\n€ 213.32"
+
+    #take_screenshot
+  end
+
+  test "Visit account" do
+    user = users(:bas)
+    password = "SomePassword123^!"
+    login_user(user, password)
+
+    account_name = "Current account"
+
+    visit "/account/" + URI.encode(account_name)
+
+    take_screenshot
+  end
+end
