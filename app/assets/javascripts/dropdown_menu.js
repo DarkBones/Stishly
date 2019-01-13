a16e76d47336eb7db8f4a7d9170f1ac2206cb0b3
DarkBones <donkerbc@gmail.com>
2019-01-13 14:30:35 +0000

    Category search box

--- app/assets/javascripts/dropdown_menu.js
+++ app/assets/javascripts/dropdown_menu.js
@@ -2,6 +2,7 @@ $(document).on('turbolinks:load', ()=> {
 
   $('.dropdown').on('click', (event) => {
     $(event.target).next('.dropdown__options').toggle();
+    $(event.target).next('.dropdown__options').find('#search-categories').focus();
   });
 
   $('.dropdown__options li').on('click', (event) => {
@@ -10,4 +11,20 @@ $(document).on('turbolinks:load', ()=> {
     $(event.target).closest('.dropdown').next('input').val($(event.target).attr('class').split('_')[1]);
   });
 
+  $('#search-categories').on('keyup', (event) => {
+    SearchCategories($(event.target).closest('.dropdown__options'), $(event.target).val(), event);
+  });
+
 });
+
+function SearchCategories($categories, text, event) {
+  li = $categories.find('li')
+  for (i = 0; i < li.length; i++){
+
+    if (li[i].innerText.toUpperCase().indexOf(text.toUpperCase()) != -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
