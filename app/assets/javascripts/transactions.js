b69f21319e782670f966ac56240c70631f0d27f5
DarkBones <donkerbc@gmail.com>
2019-01-15 18:18:32 +0000

    New transactions form

--- app/assets/javascripts/transactions.js
+++ app/assets/javascripts/transactions.js
@@ -2,4 +2,22 @@ $(document).on('turbolinks:load', ()=> {
   $('#quick-transaction-input').focus();
 
   $('#transactionmenu').show();
+
+  if ($('#transaction_multiple_transactions').is(':checked')) {
+    $('.transactions_menu_multiple').show();
+    $('.transactions_menu_single').hide();
+  } else{
+    $('.transactions_menu_multiple').hide();
+    $('.transactions_menu_single').show();
+  }
+
+  $('#transaction_multiple_transactions').change(function() {
+    if($(this).is(":checked")) {
+      $('.transactions_menu_multiple').show();
+      $('.transactions_menu_single').hide();
+    } else {
+      $('.transactions_menu_multiple').hide();
+      $('.transactions_menu_single').show();
+    }
+  });
 });
