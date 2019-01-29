f056581186a76603bcd105ec125a3c22a5e62f9c
DarkBones <donkerbc@gmail.com>
2019-01-29 12:33:34 +0000

    Show child transactions separately

--- app/models/account/get_transactions.rb
+++ app/models/account/get_transactions.rb
@@ -18,9 +18,10 @@ class Account
 
     def get_transactions
       if @account.is_real
-        transactions = @account.transactions.order(:local_datetime).reverse_order().page(@page).per_page(30)
+        transactions = @account.transactions.where(parent_id: nil).order(:local_datetime).reverse_order().page(@page).per_page(30)
       else
-        transactions = Transaction.where(user_id: @current_user.id).order(:local_datetime).reverse_order().page(@page).per_page(30)
+        #transactions = Transaction.where(user_id: @current_user.id).order(:local_datetime).reverse_order().page(@page).per_page(30)
+        transactions = @current_user.transactions.where(parent_id: nil).order(:local_datetime).reverse_order().page(@page).per_page(30)
       end
     end
 
