7a7841344b31b7ee96ec60b15081f1237103c8cf
DarkBones <donkerbc@gmail.com>
2019-02-01 12:14:15 +0000

    Tab menu for accounts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :set_current_user
+  before_action :get_accounts_currencies
 
   helper_method :user_accounts, :user_accounts_array, :user_categories_array
+
+  def get_accounts_currencies
+    @accounts_currencies = Account.get_accounts_with_currencies(current_user)
+  end
 
   def set_current_user
     User.set_current_user(current_user)
