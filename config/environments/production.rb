632dc284c07c4ac7de233e388255b2bc45d7e497
DarkBones <donkerbc@gmail.com>
2019-02-07 10:58:25 +0000

    Set mailer host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,21 +92,21 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-=begin
+
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: Rails.application.secrets.domain_name,
+    domain: 'guarded-beach-17639.herokuapp.com',
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: Rails.application.secrets.email_provider_username,
     password: Rails.application.secrets.email_provider_password
   }
-=end
+
   # ActionMailer Config
   config.action_mailer.default_url_options = { :host => 'guarded-beach-17639.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
 end
