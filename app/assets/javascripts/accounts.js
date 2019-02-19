44b18a8879b61ac51d2da4037c7ef6ce47b664f8
DarkBones <donkerbc@gmail.com>
2019-02-19 18:08:56 +0000

    Refactor

--- app/assets/javascripts/accounts.js
+++ app/assets/javascripts/accounts.js
@@ -3,11 +3,13 @@ $(document).on('turbolinks:load', ()=> {
   // Disable the submit button
   $("#accountmenu").find("input[type=submit]").attr("disabled", "disabled");
 
+  // if the create account button is clicked, show the account creation menu
   $('#create-account-button').on('click', () => {
     ToggleCardForm('#accountmenu');
     $(".accountmenu__name").focus();
   });
 
+  // set a maximum length of 50 for the account creation menu
   $('.accountmenu__name').attr('maxlength',50);
 
   // don't allow dots in input field
@@ -23,19 +25,23 @@ $(document).on('turbolinks:load', ()=> {
     }
   });
 
+  // decide whether to show the summary account
   display_summary_account();
 
 });
 
+// if one of the accounts in the left menu is clicked, navigate to that account's page
 $(document).on('click', '.account-button', (event) => {
   link_name = event.currentTarget.closest('.account-button').getAttribute('name')
   window.location.href = '/' + link_name.replace('_', '/');
 });
 
+// after all ajax requests are done, display the summary account
 $(document).ajaxComplete(function() {
   display_summary_account();
 });
 
+// if there is less than two accounts, don't show the summary accounts
 function display_summary_account() {
   if ($("#accounts_list li").length < 3) {
     $("#accounts_list li").first().hide();
@@ -50,6 +56,7 @@ function display_summary_account() {
   }
 }
 
+// gets the currently active account by checking the URL of the current page
 function get_active_account_name(){
   url_arr = window.location.pathname.split('/');
   if (url_arr[1].toLowerCase() == 'accounts') {
