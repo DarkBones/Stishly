b64993021a303e024fbd1b05a95f508acd571d9e
DarkBones <donkerbc@gmail.com>
2019-02-27 11:14:31 +0000

    Implement system parameters

--- app/models/account/get_transactions.rb
+++ app/models/account/get_transactions.rb
@@ -17,11 +17,12 @@ class Account
     private
 
     def get_transactions
+      page_length = APP_CONFIG['ui']['transactions']['page_length']
       if @account.is_real
-        transactions = @account.transactions.where(parent_id: nil).order(:local_datetime).reverse_order().includes(:children, :category).page(@page).per_page(30)
+        transactions = @account.transactions.where(parent_id: nil).order(:local_datetime).reverse_order().includes(:children, :category).page(@page).per_page(page_length)
       else
         #transactions = Transaction.where(user_id: @current_user.id).order(:local_datetime).reverse_order().page(@page).per_page(30)
-        transactions = @current_user.transactions.where(parent_id: nil).order(:local_datetime).reverse_order().includes(:children, :category).page(@page).per_page(30)
+        transactions = @current_user.transactions.where(parent_id: nil).order(:local_datetime).reverse_order().includes(:children, :category).page(@page).per_page(page_length)
       end
     end
 
