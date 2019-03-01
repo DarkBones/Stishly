7054ef25cbf315f28768ce95797fd8e844b4a87d
DarkBones <donkerbc@gmail.com>
2019-03-01 11:41:41 +0000

    Add date picker

--- app/assets/javascripts/scheduleform.js
+++ app/assets/javascripts/scheduleform.js
@@ -4,12 +4,21 @@ function changeSchedulePeriod(selectObject) {
   val = selectObject.value;
 
   switch (val.toLowerCase()) {
-    case 'weekday':
-      showWeekday();
+    case 'daily':
+      $('#scheduleform p#period').text('Days');
+      break;
+    case 'weekly':
+      $('#scheduleform p#period').text('Weeks');
+      break;
+    case 'monthly':
+      $('#scheduleform p#period').text('Months');
+      break;
+    case 'annually':
+      $('#scheduleform p#period').text('Years');
       break;
     default:
-      hideWeekday();
-      $('#scheduleform p#period').text(val);
+      //hideWeekday();
+      $('#scheduleform p#period').text('');
   }
 }
 
@@ -34,4 +43,31 @@ function advancedScheduleOptions(){
     $advancedOptions.slideDown(200);
     $advancedOptionsToggle.text('hide advanced options');
   }
+}
+
+function schedulePickDate(date) {
+  var is_active = false;
+  var $datesPicked = $('#scheduleform #schedule_dates_picked');
+  var dates = $datesPicked.val().split(' ');
+  var index = -1;
+  var value = $(date).text();
+
+  $(date).toggleClass('active');
+  is_active = $(date).hasClass('active');
+
+  if (is_active) {
+    dates.push(value);
+  } else {
+    index = dates.indexOf(value);
+    if (index > -1) {
+      dates.splice(index, 1);
+    }
+  }
+  //dates = dates.replace(/ +(?= )/g,'');
+  console.log(dates);
+  $datesPicked.val(dates.join(' '));
+}
+
+function resetScheduleMenu(){
+
 }
