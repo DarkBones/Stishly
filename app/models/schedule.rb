5c21e701227a4e65d59e659c388a946cc7e3dcfc
DarkBones <donkerbc@gmail.com>
2019-03-17 11:57:32 +0000

    Update annotations

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -21,26 +21,6 @@
 #  is_active         :boolean          default(TRUE)
 #
 
-# == Schema Information
-#
-# Table name: schedules
-#
-#  id                :bigint(8)        not null, primary key
-#  user_id           :bigint(8)
-#  transaction_id    :bigint(8)
-#  account_id        :bigint(8)
-#  start_date        :date
-#  end_date          :date
-#  period            :string(255)
-#  period_day        :integer
-#  period_occurences :integer
-#  exception_days    :string(255)
-#  exception_rule    :string(255)
-#  next_occurrence   :date
-#  created_at        :datetime         not null
-#  updated_at        :datetime         not null
-#
-
 class Schedule < ApplicationRecord
   validates :name, :start_date, :user_id, presence: true
 
