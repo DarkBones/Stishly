c25606a9be8f19e18319bab9d1ba089384bea19d
DarkBones <donkerbc@gmail.com>
2019-03-25 18:00:31 +0000

    Fix heroku migration

--- db/migrate/20190323130620_changeschedulescol.rb
+++ db/migrate/20190323130620_changeschedulescol.rb
@@ -1,5 +1,8 @@
 class Changeschedulescol < ActiveRecord::Migration[5.2]
   def change
-    change_column :schedules, :exclusion_met_day, :integer
+    #change_column :schedules, :exclusion_met_day, :integer
+    execute <<-SQL
+      ALTER TABLE schedules ALTER COLUMN exclusion_met_day TYPE integer USING (trim(exclusion_met_day)::integer);
+    SQL
   end
 end
