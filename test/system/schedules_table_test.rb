dd49e0f68d6d302348f74dae73549a9031d39d4c
DarkBones <donkerbc@gmail.com>
2019-03-27 09:29:08 +0000

    Schedules table tests

---
+++ test/system/schedules_table_test.rb
@@ -0,0 +1,33 @@
+require "application_system_test_case"
+
+class SchedulesTableTest < ApplicationSystemTestCase
+  test "Navigate to schedules index" do
+    schedule_login
+
+    assert_selector "h3", text: "Active Schedules"
+    assert_selector "h3", text: "Inactive Schedules"
+  end
+
+  test "Schedules visible" do
+    schedule_login
+
+    assert_selector 'table#schedules_active tr td', text: 'Payday'
+    assert_selector 'table#schedules_active tr td', text: 'Bonus day'
+    assert_selector 'table#schedules_active tr td', text: 'Payday CVG'
+    assert_selector 'table#schedules_active tr td', text: 'Bonus day CVG'
+
+    assert_selector 'table#schedules_active tr td', text: '28/03/2018'
+    assert_selector 'table#schedules_active tr td', text: '28/04/2018'
+    assert_selector 'table#schedules_active tr td', text: '29/03/2018'
+    assert_selector 'table#schedules_active tr td', text: '31/05/2018'
+  end
+
+  def schedule_login
+    user = users(:schedules)
+    password = "SomePassword123^!"
+    login_user(user, password)
+    page.find(".navbar-gear").click
+    click_on "Schedules"
+  end
+
+end
