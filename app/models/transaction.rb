ac4dde43c1d0f9074c2b1e3f8ce37ff7ba4f8ea0
DarkBones <donkerbc@gmail.com>
2019-04-01 18:38:53 +0100

    Bug fixes

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -88,53 +88,6 @@ class Transaction < ApplicationRecord
     transaction = CreateFromString.new(params, current_user).perform
   end
 
-  def self.create_OLD(account_id, params, current_user)
-    account_currency = Account.get_currency(Account.find(account_id))
-
-    puts params
-
-    if account_currency.iso_code != params[:currency]
-      params[:account_currency_amount] = CurrencyRate.convert(params[:amount], Money::Currency.new(params[:currency]), account_currency)
-    else
-      params[:account_currency_amount] = params[:amount]
-    end
-
-    tz = TZInfo::Timezone.get(params[:timezone])
-    params[:local_datetime] = tz.utc_to_local(Time.now)
-
-    transaction = current_user.transactions.new(params)
-    transaction.save
-
-    Account.add(current_user, account_id, params[:amount])
-  end
-
-  def self.create_OLD2(params, current_user)
-    t = self.new
-    t.description = params[:transaction][:description]
-    t.account_id = current_user.accounts.where(name: params[:transaction][:account]).take.id
-    t.currency = params[:transaction][:currency]
-    t.user_id = current_user.id
-    t.category_id = params[:transaction][:category_id]
-
-    amount = 0
-    if params[:transaction][:amount]
-      amount = params[:transaction][:amount]
-    end
-
-    t.timezone = params[:transaction][:timezone]
-
-    tz = TZInfo::Timezone.get(params[:transaction][:timezone])
-    t.local_datetime = tz.utc_to_local(Time.now)
-
-    currency = Money::Currency.new(params[:transaction][:currency])
-
-    if currency.subunit_to_unit > 0
-      amount *= currency.subunit_to_unit
-    end
-
-    t.save
-  end
-
   def self.create(params, current_user)
     CreateFromForm.new(params, current_user).perform
   end
