af63f06cb680ec55b00a1fbcc9a9cdfa8b62e273
Bas Donker <donkerbc@gmail.com>
2019-04-01 13:37:35 +0100

    Remove disableEnterKeySubmitting function

--- app/assets/javascripts/disable_enter_key_submitting.js
+++ app/assets/javascripts/disable_enter_key_submitting.js
@@ -1,8 +1,8 @@
-$(document).on('turbolinks:load', ()=> {
+/*$(document).on('turbolinks:load', ()=> {
   function disableEnterKeySubmitting() {
     var key = e.charCode || e.keyCode || 0;
     if (key == 13) {
       e.preventDefault();
     }
   }
-}
+}*/
