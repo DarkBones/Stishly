75a72e689531780314abe7d7a60dbb72113ebb0a
DarkBones <donkerbc@gmail.com>
2019-04-03 16:55:36 +0100

    Fix migrations

--- db/migrate/20190330104017_local_datetime_to_datetime.rb
+++ db/migrate/20190330104017_local_datetime_to_datetime.rb
@@ -1,5 +1,5 @@
 class LocalDatetimeToDatetime < ActiveRecord::Migration[5.2]
   def change
-    #change_column :transactions, :local_datetime, :datetime, null: false
+    change_column :transactions, :local_datetime, :datetime, null: false
   end
 end
