3244f3d3a065f686de0f72ae782412b66c2bd9fd
Bas Donker <donkerbc@gmail.com>
2019-04-03 12:01:26 +0100

    Remove redundant variables

--- app/models/account/get_transactions.rb
+++ app/models/account/get_transactions.rb
@@ -19,10 +19,10 @@ class Account
     def get_transactions
       page_length = APP_CONFIG['ui']['transactions']['page_length']
       if @account.is_real
-        transactions = @account.transactions.where(parent_id: nil).order(:local_datetime).reverse_order().includes(:children, :category).page(@page).per_page(page_length)
+        @account.transactions.where(parent_id: nil).order(:local_datetime).reverse_order().includes(:children, :category).page(@page).per_page(page_length)
       else
         #transactions = Transaction.where(user_id: @current_user.id).order(:local_datetime).reverse_order().page(@page).per_page(30)
-        transactions = @current_user.transactions.where(parent_id: nil).order(:local_datetime).reverse_order().includes(:children, :category, :account).page(@page).per_page(page_length)
+        @current_user.transactions.where(parent_id: nil).order(:local_datetime).reverse_order().includes(:children, :category, :account).page(@page).per_page(page_length)
       end
     end
 
