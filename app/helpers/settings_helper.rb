7a9e83dcbd6fdc9f26d6d68be256c511c91f27cd
Bas Donker <donkerbc@gmail.com>
2019-04-03 12:08:25 +0100

    Remove redundant variable

--- app/helpers/settings_helper.rb
+++ app/helpers/settings_helper.rb
@@ -1,7 +1,7 @@
 module SettingsHelper
   def all_currencies(hash)
     array = []
-    hash.each_with_index do |c, i|
+    hash.each do |c|
       break if c[1][:iso_code] == 'XTS'
 
       array.push(["#{c[1][:iso_code]}", c[1][:iso_code]])
