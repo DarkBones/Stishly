a81b2fa7e4b7b04aa9aad3377d8d4157adcfaace
DarkBones <donkerbc@gmail.com>
2019-04-03 18:38:22 +0100

    Fix code style issues

--- app/assets/javascripts/sortable_lists.js
+++ app/assets/javascripts/sortable_lists.js
@@ -1,18 +1,18 @@
-function makeSortableLists(class_name='.sortable-list', handle_name='.sort-handle'){
-  $(class_name).sortable({
-    cancel: '.no-sorting',
-    handle: handle_name,
+function makeSortableLists(className=".sortable-list", handleName=".sort-handle"){
+  $(className).sortable({
+    cancel: ".no-sorting",
+    handle: handleName,
     delay: 150,
     update: function(e, ui) {
       Rails.ajax({
-        url: '/accounts/sort',
-        type: 'PATCH',
-        data: $(this).sortable('serialize'),
+        url: "/accounts/sort",
+        type: "PATCH",
+        data: $(this).sortable("serialize"),
       });
     }
   });
 }
 
-$(document).on('turbolinks:load', ()=> {
+$(document).on("turbolinks:load", ()=> {
   makeSortableLists();
 });
