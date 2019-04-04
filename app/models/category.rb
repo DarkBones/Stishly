3e08ea460df35b0da914fe1bcdb63b331398046f
Bas Donker <donkerbc@gmail.com>
2019-04-04 13:31:05 +0100

    Bug fixes

--- app/models/category.rb
+++ app/models/category.rb
@@ -37,8 +37,8 @@ class Category < ApplicationRecord
         tree[cat.id][:color] = cat[:color]
         tree[cat.id][:symbol] = cat[:symbol]
         tree[cat.id][:children_paths] = cat[:name]
+        tree[cat.parent_id][:children_paths] += "_" + cat[:name]
         tree[cat.parent_id][:children].push(tree[cat.id])
-        tree[cat.parent_id][:children_paths] += cat[:name]
       end
       
       return tree[nil][:children]
