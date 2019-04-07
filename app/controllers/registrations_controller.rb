db24ae46c528c21888e729782299dacecde51717
DarkBones <donkerbc@gmail.com>
2019-04-07 10:24:45 +0100

    Add currency to backend at user creation

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,7 +3,7 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    params.require(:user).permit(:first_name, :last_name, :email, :country_code, :password, :password_confirmation, :timezone)
+    params.require(:user).permit(:first_name, :last_name, :email, :country_code, :currency, :password, :password_confirmation, :timezone)
   end
 
   def account_update_params
