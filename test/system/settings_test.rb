abda67cf7530cc13da161ca019c29888ebfae41d
DarkBones <donkerbc@gmail.com>
2019-04-07 10:29:31 +0100

    Remove change_user_currency_test

--- test/system/settings_test.rb
+++ test/system/settings_test.rb
@@ -1,28 +1,5 @@
 require "application_system_test_case"
 
 class SettingsTest < ApplicationSystemTestCase
-  test "Change user currency" do
-    """
-    Change the currency user setting
-    Expected result:
-    - See new currency selected in dropdown menu
-    - See new currency in all accounts
-    """
-
-    user = users(:bas)
-    password = "SomePassword123^!"
-
-    login_user(user, password)
-
-    page.find(".navbar-gear").click
-
-    click_on "Settings"
-
-    select "JPY", from: "setting_value_currency"
-
-    click_on "Save"
-
-    assert_selector '#accounts_list', text: "All\n¥248"
-
-  end
+  
 end
