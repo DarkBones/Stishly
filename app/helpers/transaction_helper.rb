c0a88bd56a27b1ac8d30b67c5643ab44ddd8bf0f
DarkBones <donkerbc@gmail.com>
2019-05-08 20:19:57 +0100

    New transaction form fields

--- app/helpers/transaction_helper.rb
+++ app/helpers/transaction_helper.rb
@@ -12,4 +12,47 @@ module TransactionHelper
     end
     return dates
   end
+
+  def transaction_form_params(transaction)
+    params = {}
+
+    type = ""
+    if transaction.transfer_transaction_id
+      type = "transfer"
+    elsif transaction.amount > 0
+      type = "income"
+    else
+      type = "expense"
+    end
+    params[:type] = type
+
+    params[:single_account_style] = "display: block;"
+    params[:transfer_account_style] = "display: none;"
+    if type == "transfer"
+      params[:type_bg_color] = "bg-warning"
+      params[:type_transfer_class] = "active"
+      params[:single_account_style] = "display: none;"
+      params[:transfer_account_style] = "display: block;"
+    elsif type == "income"
+      params[:type_bg_color] = "bg-success"
+      params[:type_income_class] = "active"
+    else
+      params[:type_bg_color] = "bg-danger"
+      params[:type_expense_class] = "active"
+    end
+
+    if transaction.persisted?
+      params[:from_account_name] = transaction.account.name
+      params[:to_account_name] = params[:from_account_name]
+      if transaction.transfer_account_id
+        params[:to_account_name] = current_user.accounts.find(transaction.transfer_account_id).name
+      end
+    else
+      params[:from_account_name] = Account.get_accounts(current_user)[0].name
+      params[:to_account_name] = params[:from_account_name]
+    end
+
+    return params
+  end
+
 end
