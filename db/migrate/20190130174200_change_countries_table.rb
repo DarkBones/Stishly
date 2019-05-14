2eb69c6e595604b570b8b4107addf9437d8d8b11
DarkBones <donkerbc@gmail.com>
2019-05-14 21:19:30 +0100

    Bug fixes

--- db/migrate/20190130174200_change_countries_table.rb
+++ db/migrate/20190130174200_change_countries_table.rb
@@ -1,7 +1,7 @@
 class ChangeCountriesTable < ActiveRecord::Migration[5.2]
   def change
-    execute <<-SQL
-      ALTER TABLE schedules ADD CONSTRAINT period_num CHECK (period_num > 0);
-    SQL
+    #execute <<-SQL
+      #ALTER TABLE schedules ADD CONSTRAINT period_num CHECK (period_num > 0);
+    #SQL
   end
 end
