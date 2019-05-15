9059e7d13dfe5d95e68c75a62cd7a164ac7294ae
DarkBones <donkerbc@gmail.com>
2019-05-15 17:10:41 +0100

    Seed db

--- config/deploy.rb
+++ config/deploy.rb
@@ -76,6 +76,7 @@ task :deploy do
     invoke :'deploy:link_shared_paths'
     invoke :'bundle:install'
     invoke :'rails:db_migrate'
+    invoke :'rails:db_seed'
     invoke :'rails:assets_precompile'
     invoke :'deploy:cleanup'
 
