0aed3ec3cffe5ad7a0e377b914edcdc7747f279a
Bas Donker <donkerbc@gmail.com>
2019-05-19 10:42:04 +0100

    Bug fixes

--- app/helpers/category_helper.rb
+++ app/helpers/category_helper.rb
@@ -1,21 +1,4 @@
 module CategoryHelper
-  def draw_tree_OLD(node, result="")
-    result += "<ul>"
-    node.each do |n|
-      result += "<li class=\"category_" + n[:id].to_s + " dropdown-item py-2 px-0\""
-      result += " onclick=\"setCategory(" + n[:id].to_s + ")\">"
-      result += image_tag('categories/' + n[:symbol] + '.svg', :class => 'rounded-circle', :style => 'background-color: hsl(' + n[:color] + ');', 'height' => '30')
-      result += " " + n[:name]
-      if n[:children].any?
-        result += draw_tree(n[:children])
-      end
-      result += "</li>"
-    end
-
-    result += "</ul>"
-
-    return result.html_safe
-  end
 
   def draw_tree(node, suff="")
     result = "<ul>"
