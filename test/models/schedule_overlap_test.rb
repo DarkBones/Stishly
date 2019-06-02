ad15ef6956f74d871e3a33ca9344c31800174fe5
DarkBones <donkerbc@gmail.com>
2019-06-02 10:35:48 +0100

    Tests

--- test/models/schedule_overlap_test.rb
+++ test/models/schedule_overlap_test.rb
@@ -1,5 +1,11 @@
 require 'test_helper'
 
 class ScheduleOverlapTest < ActiveSupport::TestCase
+  
+  test "overlap payday with daily" do
+    current_user = users(:schedules_overlap)
+
+    puts current_user.id
+  end
 
 end
