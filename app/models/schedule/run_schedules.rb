50ef2d4ff5c3d541d40d5faedf927497e05a8453
= <donkerbc@gmail.com>
2019-06-12 20:37:52 +0100

    Bug fixes

--- app/models/schedule/run_schedules.rb
+++ app/models/schedule/run_schedules.rb
@@ -29,6 +29,7 @@ private
     end
 
     def get_scheduled_transactions(schedule)
+      return [] unless schedule.is_active
       sch_transactions = schedule.user_transactions
       transactions = []
       sch_transactions.each do |st|
@@ -42,9 +43,18 @@ private
     def update_next_rundate(schedule)
       tz = TZInfo::Timezone.get(schedule.timezone)
 
+      # record the occurrence
+      occurrence = ScheduleOccurrence.new({
+        schedule_id: schedule.id,
+        occurrence_utc: tz.utc_to_local(schedule.next_occurrence.to_datetime),
+        occurrence_local: schedule.next_occurrence
+      })
+      occurrence.save
+
       next_occurrence = Schedule.next_occurrence(schedule, @datetime.to_date, false, true)
       schedule.next_occurrence = tz.utc_to_local(next_occurrence).to_date unless next_occurrence.nil?
       schedule.next_occurrence_utc = next_occurrence
+
 
     end
 
