191704bd922982277c43552f896179df93d097d5
= <donkerbc@gmail.com>
2019-06-24 17:31:25 +0100

    Bug fixes

--- app/models/account/get_daily_totals.rb
+++ app/models/account/get_daily_totals.rb
@@ -12,6 +12,7 @@ class Account
     def perform
       days = {}
 
+=begin
       @transactions.each do |t|
         next if t.is_scheduled == true
 
@@ -23,7 +24,7 @@ class Account
           days[day] = @current_user.transactions.where("DATE(transactions.local_datetime) = DATE(?) AND parent_id IS NULL AND is_scheduled = 0", day).sum(:user_currency_amount) unless days.keys.include? day
         end
       end
-
+=end
       return days
     end
 
