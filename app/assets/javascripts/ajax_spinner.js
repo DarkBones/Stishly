0db84d63b7dcbfaab657de11bb0d79e81e4fb9f3
= <donkerbc@gmail.com>
2019-08-24 11:54:50 +0100

    Bug fixes

--- app/assets/javascripts/ajax_spinner.js
+++ app/assets/javascripts/ajax_spinner.js
@@ -1,20 +1,20 @@
 var spinnerHtml;
 
+$(function() {
+
+	window.insertAjaxSpinner = function($target, size=75) {
+		if ($target.find(".ajax_spinner").length === 0){
+			$target.prepend(spinnerHtml);
+			$target.find(".ajax_spinner img").css("height", size.toString() + "px");
+			$target.find(".ajax_spinner").show();
+		}
+	};
+	window.removeAjaxSpinner = function($target) {
+		$target.find(".ajax_spinner").remove();
+	};
+
+});
+
 $(document).on("turbolinks:load", () => {
 	spinnerHtml = $("#ajax_spinner_template").html();
-
-	$(function() {
-
-		window.insertAjaxSpinner = function($target, size=75) {
-			if ($target.find(".ajax_spinner").length === 0){
-				$target.prepend(spinnerHtml);
-				$target.find(".ajax_spinner img").css("height", size.toString() + "px");
-				$target.find(".ajax_spinner").show();
-			}
-		};
-		window.removeAjaxSpinner = function($target) {
-			$target.find(".ajax_spinner").remove();
-		};
-
-	});
 });
