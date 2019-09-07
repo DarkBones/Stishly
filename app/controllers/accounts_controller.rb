44cfd20e90e72467b78cf088166ea0b5cf1ff059
= <donkerbc@gmail.com>
2019-09-07 10:58:13 +0200

    Remove old functions

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,7 +1,7 @@
 class AccountsController < ApplicationController
 
   def destroy
-    Account.delete(current_user.accounts.find(params[:id]))
+    Account.delete(current_user.accounts.friendly.find(params[:id]))
     redirect_to "/accounts", notice: "Account deleted"
   end
 
@@ -80,33 +80,6 @@ class AccountsController < ApplicationController
     render 'show'
   end
 
-  def details
-    @account = current_user.accounts.where(name: params[:id]).take.decorate
-    render json: {
-      id: @account.id,
-      balance: @account.balance,
-      balance_float: @account.balance_float,
-      created_at: @account.created_at,
-      updated_at: @account.updated_at,
-      name: @account.name,
-      description: @account.description,
-      currency: @account.currency
-    }
-  end
-
-  def format_currency
-    render json: Account.format_currency(params[:amount], params[:currency])
-  end
-
-  def convert_currency
-    render json: CurrencyRate.convert(params[:amount], Money::Currency.new(params[:from]), Money::Currency.new(params[:to]))
-  end
-
-  def all_details
-    @accounts = Account.get_accounts(current_user)
-    render json: @accounts
-  end
-
   def create
     @account = Account.create_new(new_account_params, current_user)
 
@@ -132,7 +105,7 @@ class AccountsController < ApplicationController
   end
 
   def edit
-    account = current_user.accounts.find(params[:id])
+    account = current_user.accounts.friendly.find(params[:id])
     Account.change_setting(account, account_settings_params)
 
     redirect_back(fallback_location: root_path)
