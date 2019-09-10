42b09a1c066c240f3bdc56a40814540fdf5fc221
= <donkerbc@gmail.com>
2019-09-10 14:37:20 +0200

    Nested parent colors

--- app/decorators/category_decorator.rb
+++ app/decorators/category_decorator.rb
@@ -2,12 +2,23 @@ class CategoryDecorator < ApplicationDecorator
   delegate_all
 
   def color
-    return model.color unless model.color.nil?
-    return "0, 0%, 50%" if model.parent_id.nil?
-    return "0, 0%, 50%" if Category.find(model.parent_id).nil?
-    return "0, 0%, 50%" if Category.find(model.parent_id).color.nil?
+  	return model.color unless model.color.nil?
 
-    return Category.find(model.parent_id).color
+  	unless model.parent_id.nil?
+  		parent_id = model.parent_id
+  		while !parent_id.nil?
+  			parent = Category.find(parent_id)
+
+  			unless parent.color.nil?
+  				return parent.color
+  				break
+  			end
+
+  			parent_id = parent.parent_id
+  		end
+  	end
+
+  	return "0, 0%, 50%"
   end
 
 end
