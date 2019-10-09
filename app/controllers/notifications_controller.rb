ae97f28b6a85f5246ee0d8cc93ddeaf18bbc28aa
= <donkerbc@gmail.com>
2019-10-09 19:26:35 -0400

    Manage subscription link

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,7 +2,7 @@ class NotificationsController < ApplicationController
 
 	def show
 		@truncate = false
-		@notifications = current_user.notifications.reverse_order.paginate(page: params[:page], per_page: APP_CONFIG['ui']['notifications']['page_length'])
+		@notifications = current_user.notifications.reverse_order.paginate(page: params[:page], per_page: APP_CONFIG['ui']['notifications']['page_length']).decorate
 	end
 
 end
