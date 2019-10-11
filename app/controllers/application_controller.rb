1531976afa9760f248f9078e7075013c0b5d3185
= <donkerbc@gmail.com>
2019-10-11 18:17:56 -0400

    Bug fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -60,7 +60,13 @@ class ApplicationController < ActionController::Base
         #@budget = User.daily_budget(current_user)
       end
     end
-    #@budget = User.daily_budget(current_user)
+    @budget = {
+        type: 'days',
+        days: 1337,
+        balance: user.accounts.length,
+        transactions_total: scheduled_transactions[:total] * 4,
+        average_spending: 10000000000000000000
+      }
   end
 
   def privacy_policy
