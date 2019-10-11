38519354db290821b62f46ddc10a82073444870f
= <donkerbc@gmail.com>
2019-10-11 13:58:52 -0400

    Bug fixes

--- config/deploy.rb
+++ config/deploy.rb
@@ -76,7 +76,7 @@ task :deploy do
     invoke :'deploy:link_shared_paths'
     invoke :'bundle:install'
     invoke :'rails:db_migrate'
-    invoke :'rails:assets_precompile'
+    #invoke :'rails:assets_precompile'
     invoke :'deploy:cleanup'
 
     on :launch do
