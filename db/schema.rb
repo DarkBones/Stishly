914331293af72fd74bcbd41350c34e9c7b0a9b8c
= <donkerbc@gmail.com>
2019-10-11 11:20:50 -0400

    Database migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_09_183114) do
+ActiveRecord::Schema.define(version: 2019_10_11_152007) do
 
   create_table "account_histories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "account_id"
