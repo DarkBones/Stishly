f0ad494cdb3e81a3ae89caa31fc663c52e6d9ea8
= <donkerbc@gmail.com>
2019-11-02 11:20:56 -0400

    Confirmation email

--- app/mailers/sendgrid_mailer.rb
+++ app/mailers/sendgrid_mailer.rb
@@ -7,10 +7,10 @@ class SendgridMailer < Devise::Mailer
   	subs = {
   		title: "Welcome to Stishly",
   		greeting: "Hi #{record.first_name},",
-  		message: "Thank you for signing up! Please confirm your email address by clicking the button below",
+  		message: "Thank you for signing up! Please confirm your email address by clicking the button below.",
   		confirmText: "Confirm My Account",
   		token: token,
-  		confirmationUrl: "https://stishly.com/users/confirmation?confirmation_token=#{token}",
+  		confirmationUrl: "#{root_url}users/confirmation?confirmation_token=#{token}",
   	}
 	  #Sendgrid.send(record.email, subs, "d-46d71cf815ac42a1897b6353684fa831")
 	  Sendgrid.send("donkerbc@gmail.com", subs, "d-46d71cf815ac42a1897b6353684fa831")
