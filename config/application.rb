a26adb371bfb6076700930cefd188d5b14b92bcc
= <donkerbc@gmail.com>
2019-11-05 13:05:15 -0500

    Form helpers

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,8 @@ module MoneyManager
     config.autoload_paths += %W(#{config.root}/lib)
     config.exceptions_app = self.routes
 
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
