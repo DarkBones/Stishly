7c1f6161872c65d07d7e3f8f4dd408ba64db1d66
= <donkerbc@gmail.com>
2019-11-11 17:34:28 -0500

    Raise exceptions for missing I18n

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -49,5 +49,5 @@ Rails.application.configure do
   #config.assets.prefix = ''
 
   # Raises error for missing translations
-  # config.action_view.raise_on_missing_translations = true
+  config.action_view.raise_on_missing_translations = true
 end
