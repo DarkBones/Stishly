f66479c5415e148e638fcdc830d2461d29f4c4d5
= <donkerbc@gmail.com>
2019-11-22 10:17:02 -0500

    Bug fixes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,18 @@
 class SessionsController < Devise::SessionsController
   respond_to :json
 
+  def create
+    self.resource = warden.authenticate!(auth_options)
+  end
+
   private
 
-  def respond_with(resource, _opts = {})
+  def respond_with_old(resource, _opts = {})
     render json: resource
   end
 
-  def respond_to_on_destroy
+  def respond_to_on_destroy_old
     head :no_content
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -481,6 +481,12 @@ Rails.application.routes.draw do
 
   #devise_for :users, :controllers => { registrations: 'registrations', omniauth_callbacks: 'users/omniauth_callbacks' }
   devise_for :users,
+            path: '',
+            path_names: {
+              sign_in: 'login',
+              sign_out: 'logout',
+              registration: 'signup'
+            },
              controllers: {
                sessions: 'sessions',
                registrations: 'registrations'
